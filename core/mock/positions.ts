6e939988110218ed677811b445479a8d943b1e40
Lucas <lukasantosilva@yahoo.com.br>
2024-10-04 16:32:23 -0300

    add local

--- core/mock/positions.ts
+++ core/mock/positions.ts
@@ -1,4 +1,4 @@
-export const MOCK_LOCALS = ['Guichê', 'Sala', 'Recepção', 'Consultório']
+export const MOCK_LOCALS = ['Guichê', 'Sala', 'Recepção', 'Consultório', 'Ambulatório']
 export const MOCK_POSITIONS = Array(30)
   .fill(null)
   .map((_, index) => (index + 1).toString().padStart(2, '0'))
